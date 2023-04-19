2ed6f1bfb9af68ed22346e518a384caaa3d87a88
Manoj Paudel <109033830+sudomanoj@users.noreply.github.com>
2023-04-19 19:36:58 +0545

    Update forms.py

--- blog/forms.py
+++ blog/forms.py
@@ -27,7 +27,7 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'desc']
-        labels = {'title': "Title", 'desc':"DEscription"}
+        labels = {'title': "Title", 'desc':"Description"}
         widgets = {
             'title':forms.TextInput(attrs={'class': 'form-control'}),
             'desc':forms.Textarea(attrs={'class': 'form-control'}),
